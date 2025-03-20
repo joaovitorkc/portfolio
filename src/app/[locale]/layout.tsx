53f4ddf974097eabedf9909f83a0643a778eeaba
joaovitorkc <jv0488598@gmail.com>
2025-03-20 15:17:35 -0300

    link fix

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from '@/components/ui/sonner';
 import { NextIntlClientProvider, hasLocale } from 'next-intl';
 import { notFound } from 'next/navigation';
 import { routing } from '@/i18n/routing';
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ['latin'] });
